57081341b6615714736782b75d96fea1ba7142be
Yuval Adam <_@yuv.al>
2020-09-26 18:24:43 +0300

    Add jinja2 templates support

--- iss/server.py
+++ iss/server.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="iss/templates")
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def home(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
