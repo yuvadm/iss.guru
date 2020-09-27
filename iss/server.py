1a1a73f21a9552f4657565cabcb83d3bb9321e17
Yuval Adam <_@yuv.al>
2020-09-27 11:44:06 +0300

    Add static files and base template

--- iss/server.py
+++ iss/server.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="iss/static"), name="static")
 
 templates = Jinja2Templates(directory="iss/templates")
 
